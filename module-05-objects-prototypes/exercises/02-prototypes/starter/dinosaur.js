8af6aff8a68214b58a126599095b61d2c8fd71ac
AmyOctoCat <amy.pike@bbc.co.uk>
2026-04-16 11:29:44 +0100

    second exercise

--- module-05-objects-prototypes/exercises/02-prototypes/starter/dinosaur.js
+++ module-05-objects-prototypes/exercises/02-prototypes/starter/dinosaur.js
@@ -3,14 +3,24 @@
  */
 
 export class Dinosaur {
-  constructor() {
-    throw new Error('TODO');
+  constructor(name, species, zone) {
+    this.name = name;
+    this.species = species;
+    this.zone = zone;
+  }
+
+  describe() {
+    return `${this.name} - ${this.species} @ ${this.zone}`
   }
 }
 
 export class FlyingDinosaur extends Dinosaur {
-  constructor() {
-    super();
-    throw new Error('TODO');
+  constructor(name, species, zone, wingspanM) {
+    super(name, species, zone);
+    this.wingspanM = wingspanM;
+  }
+
+  describe() {
+    return `${super.describe()} - wingspan ${this.wingspanM}m`;
   }
 }
